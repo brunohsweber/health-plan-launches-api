caaef2f6764863365c2c53d22195cedd736e068c
Bruno Weber <contato@brunoweber.com.br>
2022-03-04 16:10:06 -0300

    build(AppError): add file to handle error

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,11 +1,26 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express";
+
 import "express-async-errors";
+import { AppError } from "@shared/errors/AppError";
 
 const app = express();
 
 app.use(express.json());
 
-console.log("Configurações do servidor ok!");
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
 
 export { app };
